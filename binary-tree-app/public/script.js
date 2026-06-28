5d2f311ae8ab9f3c4ff96ac9bfaf4f0d6952abe5
alprok8922399-stack <alprok8922399@gmail.com>
2026-06-28 18:59:31 +0500

    Enhance renderNode function with level classes

--- binary-tree-app/public/script.js
+++ binary-tree-app/public/script.js
@@ -3,30 +3,27 @@ fetch('/data.json')
     .then(data => {
         const treeDiv = document.getElementById('tree');
         
-        // Вспомогательная функция для отрисовки узла или пустой рамки
-        const renderNode = (user, label) => `
-            <div class="node">
-                ${label}<br>
-                <b>${user ? user.login : 'Свободно'}</b>
-            </div>
+        // Функция с добавлением классов уровней
+        const renderNode = (user, label, levelClass) => `
+            <div class="node ${levelClass}">${label}<br><b>${user ? user.login : '---'}</b></div>
         `;
 
         treeDiv.innerHTML = `
             <div class="branch">
-                ${renderNode(data, 'Admin')}
+                ${renderNode(data, 'Admin', 'level-1')}
                 <div class="children">
                     <div class="branch">
-                        ${renderNode(data.left, 'Left 1')}
+                        ${renderNode(data.left, 'L1', 'level-2')}
                         <div class="children">
-                            ${renderNode(data.left?.left, 'L-L')}
-                            ${renderNode(data.left?.right, 'L-R')}
+                            ${renderNode(data.left?.left, 'LL', 'level-3')}
+                            ${renderNode(data.left?.right, 'LR', 'level-3')}
                         </div>
                     </div>
                     <div class="branch">
-                        ${renderNode(data.right, 'Right 1')}
+                        ${renderNode(data.right, 'R1', 'level-2')}
                         <div class="children">
-                            ${renderNode(data.right?.left, 'R-L')}
-                            ${renderNode(data.right?.right, 'R-R')}
+                            ${renderNode(data.right?.left, 'RL', 'level-3')}
+                            ${renderNode(data.right?.right, 'RR', 'level-3')}
                         </div>
                     </div>
                 </div>
