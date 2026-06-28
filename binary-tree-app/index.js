98531d3bdeb38803122c009cc27c912ae4c4f0e9
alprok8922399-stack <alprok8922399@gmail.com>
2026-06-28 19:31:29 +0500

    Implement recursive user addition in binary tree

--- binary-tree-app/index.js
+++ binary-tree-app/index.js
@@ -6,25 +6,34 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-// Путь к базе данных
 const DATA_FILE = path.join(__dirname, 'data.json');
 
-// Метод для записи нового пользователя в первую свободную ячейку
+// Рекурсивная функция для поиска первой пустой ячейки
+function findAndFill(node, login) {
+    if (!node.left) {
+        node.left = { login: login, left: null, right: null };
+        return true;
+    }
+    if (findAndFill(node.left, login)) return true;
+    
+    if (!node.right) {
+        node.right = { login: login, left: null, right: null };
+        return true;
+    }
+    return findAndFill(node.right, login);
+}
+
 app.post('/add-user', (req, res) => {
     const { login } = req.body;
     let data = JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
 
-    // Простейшая логика: ищем, кто пустой (null)
-    if (!data.left) {
-        data.left = { login: login, left: null, right: null };
-    } else if (!data.right) {
-        data.right = { login: login, left: null, right: null };
+    // Запускаем поиск места, начиная с Admin
+    if (findAndFill(data, login)) {
+        fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+        res.send({ success: true });
     } else {
-        return res.status(400).send('Структура заполнена!');
+        res.status(400).send('Мест нет!');
     }
-
-    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
-    res.send({ success: true });
 });
 
-app.listen(3000, () => console.log('Сервер запущен на порту 3000'));
+app.listen(3000, () => console.log('Сервер запущен. Дерево растет бесконечно!'));
