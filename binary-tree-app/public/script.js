3346bb869eb707ef33f0e4ec6003dcfdd4743d6d
alprok8922399-stack <alprok8922399@gmail.com>
2026-06-28 18:43:49 +0500

    Refactor renderNode function for clarity

--- binary-tree-app/public/script.js
+++ binary-tree-app/public/script.js
@@ -2,31 +2,25 @@ fetch('/data.json')
     .then(response => response.json())
     .then(data => {
         const treeDiv = document.getElementById('tree');
-        
-        // Вспомогательная функция для отрисовки узла или пустой рамки
         const renderNode = (user, label) => `
-            <div class="node">
-                ${label}<br>
-                <b>${user ? user.login : 'Свободно'}</b>
-            </div>
+            <div class="node">${label}<br><b>${user ? user.login : '---'}</b></div>
         `;
-
         treeDiv.innerHTML = `
             <div class="branch">
                 ${renderNode(data, 'Admin')}
                 <div class="children">
                     <div class="branch">
-                        ${renderNode(data.left, 'Left 1')}
+                        ${renderNode(data.left, 'L1')}
                         <div class="children">
-                            ${renderNode(data.left?.left, 'L-L')}
-                            ${renderNode(data.left?.right, 'L-R')}
+                            ${renderNode(data.left?.left, 'LL')}
+                            ${renderNode(data.left?.right, 'LR')}
                         </div>
                     </div>
                     <div class="branch">
-                        ${renderNode(data.right, 'Right 1')}
+                        ${renderNode(data.right, 'R1')}
                         <div class="children">
-                            ${renderNode(data.right?.left, 'R-L')}
-                            ${renderNode(data.right?.right, 'R-R')}
+                            ${renderNode(data.right?.left, 'RL')}
+                            ${renderNode(data.right?.right, 'RR')}
                         </div>
                     </div>
                 </div>
